9ee2a76759d0277909444cea608387a05225d0cd
Claud0505 <37052240+Claud9855@users.noreply.github.com>
2022-12-28 21:08:47 -0500

    added feedback get route

--- server.js
+++ server.js
@@ -14,8 +14,19 @@ app.use('/api', api)
 
 app.use(express.static('public'))
 
+//Get route for homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+// Get route for feedback page
+app.get('/feedback', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/pages/feedback.html'))
+})
+
+// wildcard route to direct users to a 404 page
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/pages/404.html'))
 })
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`))
