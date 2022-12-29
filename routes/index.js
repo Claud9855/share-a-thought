3d78de73f8e0636133abed00cdcb71458543d972
Claud0505 <37052240+Claud9855@users.noreply.github.com>
2022-12-28 21:11:49 -0500

    added feedback router to middleware

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
 const thoughtRouter = require('./thoughts')
+const feedbackRouter = require('./feedback')
 
 const app = express()
 
 app.use('/thoughts', thoughtRouter)
+app.use('/feedback', feedbackRouter)
 
 module.exports = app
