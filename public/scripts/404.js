b924180f8165fe4db71056c8b037bea68b3f8147
Claud0505 <37052240+Claud9855@users.noreply.github.com>
2022-12-28 23:13:49 -0500

    implemented feedback script

--- public/scripts/404.js
+++ public/scripts/404.js
@@ -4,7 +4,7 @@
 const goBackBtn = document.getElementById('back')
 
 goBackBtn.addEventListener('click', (e) => {
-    e.preventDefault
+    e.preventDefault()
     // Built-in browser interface that allows changing the browser session history, or in this case to go back
     history.back()
 })
---
+++ public/scripts/feedback.js
@@ -0,0 +1,39 @@
+const feedbackForm = document.getElementById('feedback-form')
+const homeBtn = document.getElementById('home-btn')
+
+homeBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    window.location.href = '/'
+})
+
+if(feedbackForm) {
+    feedbackForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        let feedback = document.getElementById('feedbackText').value
+        let email = document.getElementById('feedbackUsername').value.trim()
+
+        const newFeedback = {
+            feedback,
+            email,
+            feedbackType: 'Complaint'
+        }
+
+        fetch('api/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(newFeedback)
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            alert(data.status)
+            email = ''
+            feedback = ''
+        })
+    })
+    .catch((error) => {
+        console.log('Error:', error)
+    })
+}
