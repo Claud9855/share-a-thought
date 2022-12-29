97b38912f7d86624d2b4a5a4706a7d0e149e7b79
Claud0505 <37052240+Claud9855@users.noreply.github.com>
2022-12-28 21:09:47 -0500

    implemented feedback router with feedback page created as well with the 404 page too

---
+++ routes/feedback.js
@@ -0,0 +1,36 @@
+const feedback = require('express').Router()
+const {v4: uuidv4} = require('uuid')
+const {readAndAppend, readFromFile} =require('../helpers/fsUtils')
+
+// GET route for retrieving all the feedback
+feedback.get('/', (req, res) => {
+    readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)))
+})
+
+// POST route for submitting a feedback
+feedback.post('/', (req, res) => {
+    const {email, feedbackType, feedback} = req.body
+
+    if(email && feedbackType && feedback) {
+        const newFeedBack = {
+            email,
+            feedbackType,
+            feedback,
+            feedback_id: uuidv4()
+        }
+
+        readAndAppend(newFeedBack, './db/feedback.json')
+
+        const response = {
+            status: 'success',
+            body: newFeedBack
+        }
+
+        res.json(response)
+    }
+    else {
+        res.json('Error in posting feedback')
+    }
+})
+
+module.exports = feedback
